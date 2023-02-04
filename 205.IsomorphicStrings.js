180d05dd42338b03b0537ef526d2ddb9ed37ed1b
Mehmet Samet Gok <msametgok@hotmail.com>
2023-02-03 23:39:25 -0500

    Isomorphic Strings

---
+++ 205.IsomorphicStrings.js
@@ -0,0 +1,72 @@
+/*
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving
+the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Input: s = "paper", t = "title"
+Output: true
+
+Input: s = "foo", t = "bar"
+Output: false
+ */
+
+
+//Time Complexity : O(N)  78.55% - Space Comp : O(N) - 61.85%
+//ChatGpt Kodu
+function isIsomorphic(s, t) {
+    if (s.length !== t.length) return false;
+
+    const mapS = new Array(256).fill(-1);
+    const mapT = new Array(256).fill(-1);
+
+    for (let i = 0; i < s.length; i++) {
+        if (mapS[s.charCodeAt(i)] !== -1) {
+            if (mapS[s.charCodeAt(i)] !== t.charCodeAt(i)) return false;
+        } else {
+            if (mapT[t.charCodeAt(i)] !== -1) return false;
+            mapS[s.charCodeAt(i)] = t.charCodeAt(i);
+            mapT[t.charCodeAt(i)] = s.charCodeAt(i);
+        }
+    }
+    return true;
+}
+
+//ChatGpt 2. Kod
+//Time Complexity O(N) Space Com : O(1)
+function isIsomorphic2(s, t) {
+    if (s.length !== t.length) return false;
+
+    const mapS = new Array(256).fill(-1);
+    const mapT = new Array(256).fill(-1);
+
+    for (let i = 0; i < s.length; i++) {
+        if (mapS[s.charCodeAt(i)] !== -1) {
+            if (mapS[s.charCodeAt(i)] !== t.charCodeAt(i)) return false;
+        } else {
+            if (mapT[t.charCodeAt(i)] !== -1) return false;
+            mapS[s.charCodeAt(i)] = t.charCodeAt(i);
+            mapT[t.charCodeAt(i)] = s.charCodeAt(i);
+        }
+    }
+    return true;
+}
+
+//LeetCode En hizli kod
+// Time Comp: O(N*2) Hiz : 99.2% -Space Comp: O(1) Hafiza : 96.50%
+//Cahtgptye gore diger 2 kod bundan daha iyi cunku bunun time comp o(n*2)
+//ama LeetCode en hizli ve hafizasi baya iyi. Sanirim kucuk stringlerde daha efektif calisiyor
+//ama ustteki kod buyuk stringler icin efektif calisiyor
+function isIsomorphic3 (s, t) {
+    if (s.length !== t.length) return false;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) !== t.indexOf(t[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isIsomorphic2('foo','bar'));
