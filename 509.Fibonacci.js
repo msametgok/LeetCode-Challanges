e64663f2a75f2ba70ab55b14d407aacca4dc40a9
Mehmet Samet Gok <msametgok@hotmail.com>
2023-03-15 22:06:25 -0400

    Fibonacci

--- 509.Fibonacci.js
+++ 509.Fibonacci.js
@@ -1,14 +1,16 @@
+// ZORLUK SEVIYESI - EASY
 
-/**
-Time Comp:
- */
+// Time Complexity : O(2^n) 14.47% ASIRI YAVAS
+// Space Comp : O(n) 96.75%
 function fib (n) {
 
     return n < 2 ? n : fib(n-1) + fib(n-2)
 
-
 }
 
+// Time Comp: O(n) 75.95% - Space Complexity: O(n) 84.10%
+//Chat Gpt kodu - En etkili yollardan birisi
+//DYNAMIC PROGRAMMING APPROACH
 function fib2 (n) {
     let dp = new Array(n+1);
     dp[0] = 0;
@@ -19,14 +21,20 @@ function fib2 (n) {
     return dp[n];
 }
 
+// Time Comp: O(n) 86.39% - Space Complexity: O(1) 67.48%
+//Chat Gpt kodu - En etkili yollardan birisi
+// ITERATIVE APPROACH
 function fib3(n) {
-    let a = 0, b = 1, c = 1;
-    for (let i = 2; i <= n; i++) {
-      c = a + b;
-      a = b;
-      b = c;
-    }
-    return c;
+  if (n === 0) return 0;
+
+  let a = 0, b = 1;
+  for (let i = 2; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
   }
 
-console.log(fib3(3));
+  return b;
+  }
+
+console.log(fib3(4));
